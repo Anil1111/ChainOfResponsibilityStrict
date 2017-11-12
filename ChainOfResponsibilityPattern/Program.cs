487b6d18954dad1de4fc8e84996a91688bee79d1
Eidivandi <Eidivandi@live.com>
2017-11-12 03:26:12 +0100

    Ajoutez des fichiers projet.

---
+++ ChainOfResponsibilityPattern/Program.cs
@@ -0,0 +1,29 @@
+﻿using ChaintOfResponsibilityCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibilityPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChainTeacherHandler teacher = new ChainTeacherHandler();
+            ChainStudentHandler student = new ChainStudentHandler();
+            ChainAnimatorHandler animator = new ChainAnimatorHandler();
+
+            teacher.ConfigurHandler(student);
+            animator.ConfigurHandler(student);
+           
+
+            Console.WriteLine(teacher.DoResponsibility(true));
+            Console.WriteLine(animator.DoResponsibility(true));
+
+            animator.ConfigurHandler(teacher);
+            Console.WriteLine(animator.DoResponsibility(true));
+        }
+    }
+}
